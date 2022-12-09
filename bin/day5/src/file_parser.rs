f7223491b55bdeffc82326113f41b4f5bf5303f1
th7mo <dev@th7mo.com>
2022-12-09 15:20:48 +0100

    make project compatible with linux

--- bin/day5/src/file_parser.rs
+++ bin/day5/src/file_parser.rs
@@ -3,12 +3,15 @@ use crate::instruction::Instruction;
 
 pub fn get_depot_and_instructions() -> (Depot, Vec<Instruction>) {
     let file = include_str!("input.txt");
-    let delimiter = "\r\n 1   2   3   4   5   6   7   8   9 \r\n\r\n";
+    let delimiter = " 1   2   3   4   5   6   7   8   9 ";
     let Some((depot_lines, instruction_lines)) = file.split_once(delimiter) else {
         panic!("incorrect input file, missing newline and 1..=9 count");
     };
 
-    (get_depot(depot_lines), get_instructions(instruction_lines))
+    (
+        get_depot(depot_lines.trim_end()),
+        get_instructions(instruction_lines.trim_start()),
+    )
 }
 
 fn get_depot(depot_str: &str) -> Depot {
